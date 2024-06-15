1cd1b20b596e3be5832f886e676c2606985c49e8
YJWG <YJWGitHub@gmail.com>
2024-06-16 04:17:37 +0900

    앨범 랜덤

---
+++ YJWHome/Components/Pages/Album.razor.cs
@@ -0,0 +1,26 @@
+﻿namespace YJWHome.Components.Pages
+{
+    public static class ExtensionFileInfo
+    {
+        private static Random _ran = new Random();
+
+        public static void Shuffle<T>(this FileInfo[] list)
+        {
+            int to = list.Length;
+            while (to > 1)
+            {
+                int from = _ran.Next(--to + 1);
+                FileInfo tmp = list[from];
+                list[from] = list[to];
+                list[to] = tmp;
+            }
+        }
+
+        public static void Swap<T>(this FileInfo[] list, int from, int to)
+        {
+            FileInfo tmp = list[from];
+            list[from] = list[to];
+            list[to] = tmp;
+        }
+    }
+}
